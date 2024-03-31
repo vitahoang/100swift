dc1ce7bc1b4970dd43e3175c0c8d4ecccf8f2cce
VitaHoang <hvhung.oc@gmail.com>
2024-03-31 14:25:15 +0700

    feat: WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vita Hoang on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
